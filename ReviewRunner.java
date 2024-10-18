f89dca68a9f5228063935f804708eff22bf32f4a
SahasKoka <151975426+SahasKoka@users.noreply.github.com>
2024-10-18 13:53:54 -0700

    Add files via upload

---
+++ ReviewRunner.java
@@ -0,0 +1,7 @@
+class ReviewRunner {
+  public static void main(String[] args) 
+  {
+    String reviewFile = "ConsumerLab_Code\\moviereviews.txt"; 
+    Review.analyzeMovieReviews(reviewFile);
+  }
+}
